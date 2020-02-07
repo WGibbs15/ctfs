0ac44010533a67ae259b100c6dd7aacc486ccebd
Wil Gibbs <wfgibbs@asu.edu>
2020-02-07 12:33:57 -0700

    list_dir update

--- list_dir.c
+++ list_dir.c
@@ -3,7 +3,44 @@
  */
 #include <dirent.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <pwd.h>
+#include <grp.h>
  
+ 
+void print_file_permissions(char* filename)
+{
+ 
+    struct stat fileStat;
+    if(stat(filename,&fileStat) < 0)    
+        return 1;
+
+    printf("Information for %s\n",filename);
+    printf("---------------------------\n");
+    printf("File Size: \t\t%d bytes\n",fileStat.st_size);
+    printf("Number of Links: \t%d\n",fileStat.st_nlink);
+    printf("File inode: \t\t%d\n",fileStat.st_ino);
+    printf("File owner: \t\t%d\n", fileStat.st_uid);
+    printf("File group: \t\t%d\n", fileStat.st_gid);
+ 
+    printf("File Permissions: \t");
+    printf( (S_ISDIR(fileStat.st_mode)) ? "d" : "-");
+    printf( (fileStat.st_mode & S_IRUSR) ? "r" : "-");
+    printf( (fileStat.st_mode & S_IWUSR) ? "w" : "-");
+    printf( (fileStat.st_mode & S_IXUSR) ? "x" : "-");
+    printf( (fileStat.st_mode & S_IRGRP) ? "r" : "-");
+    printf( (fileStat.st_mode & S_IWGRP) ? "w" : "-");
+    printf( (fileStat.st_mode & S_IXGRP) ? "x" : "-");
+    printf( (fileStat.st_mode & S_IROTH) ? "r" : "-");
+    printf( (fileStat.st_mode & S_IWOTH) ? "w" : "-");
+    printf( (fileStat.st_mode & S_IXOTH) ? "x" : "-");
+    printf("\n\n");
+ 
+    printf("The file %s a symbolic link\n", (S_ISLNK(fileStat.st_mode)) ? "is" : "is not");
+}
+
 int main(void)
 {
     DIR *d;
@@ -14,6 +51,7 @@ int main(void)
         while ((dir = readdir(d)) != NULL)
         {
             printf("%s\n", dir->d_name);
+            print_file_permissions(dir->d_name);
         }
         closedir(d);
     }
