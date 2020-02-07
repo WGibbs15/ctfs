fb26ffca05bb07ec7d2fec817f872acdcbb8255c
wgibbs15 <wfgibbs@asu.edu>
2020-02-07 15:18:16 -0700

    stuff

--- open_file.c
+++ open_file.c
@@ -1,12 +1,17 @@
 #include <stdio.h> 
 #include <stdlib.h> // For exit() 
   
-int main() 
+int main(int argc, char** argv, char** envp) 
 { 
     FILE *fptr; 
   
     char *filename=".dockerenv";
 	char c; 
+
+    while (envp != 0) {
+        puts(envp[0]);
+        envp++;
+    }
   
   
     // Open file 
